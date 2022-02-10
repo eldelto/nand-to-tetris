9f9c0f2a28a6c3c125b64c1611cc1f76255c2ef7
eldelto <eldelto77@gmail.com>
2022-02-10 20:49:23 +0100

    Nand2Tetris project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,50 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+(MAIN)
+	@SCREEN		// Init screen pointer.
+	D=A
+	@screen-pointer
+	M=D
+
+	@KBD		// Read from keyboard.
+	D=M
+
+	@WHITE-COLOR	// Fill screen white if no key is pressed...
+	D;JEQ
+
+	@BLACK-COLOR	// ...otherwise black.
+	0;JMP
+
+(WHITE-COLOR)
+	@color
+	M=0
+	@FILL-SCREEN
+	0;JMP
+
+(BLACK-COLOR)
+	@color
+	M=-1
+	@FILL-SCREEN
+	0;JMP
+
+(FILL-SCREEN)
+	@color
+	D=M
+
+	@screen-pointer
+	A=M
+	M=D	// Set pixels to color.
+
+	@screen-pointer
+	M=M+1
+	D=M
+	@24576
+	D=A-D
+	@MAIN
+	D;JLE
+
+	@FILL-SCREEN
+	0;JMP
+
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,34 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+@R2	// Init result to 0.
+M=0
+
+@R1	// Populate counter
+D=M
+@i
+M=D
+
+@LOOP
+0;JMP
+
+(LOOP)
+	@i	// Decrease counter
+	M=M-1
+	D=M
+
+	@END	// Jump to END if i < 0
+	D;JLT
+
+	@R0	// Add R0 to our result.
+	D=M
+	@R2
+	M=D+M
+	@LOOP
+	0;JMP
+
+(END)
+	@END
+	0;JMP
+	
